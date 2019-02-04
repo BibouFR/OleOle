1f1021d2db86b22f50a3f66093a2a11a5b5d247d
arnoultg <gregoire.arnoult@free.fr>
2019-02-04 15:42:48 +0100

    da

--- acceuil.py
+++ acceuil.py
@@ -1,17 +1,32 @@
 import pygame
 from pygame.locals import *
 
+class Boutton(object):
+    police = pygame.font
+    boutton = pygame.image.load("image/)
+    rect = boutton.get_rect()
+
+    def __init__(self, lettre, position):
+        self.image = Boutton.boutton.copy()
+        self.rect = Boutton.rect.copy()
+        lettre = Boutton.police.render(lettre, True, (255, 255, 255))
+        rect_lettre = lettre.get_rect(center=Boutton.rect.center)
+        self.image.blit(lettre, rect_lettre)
+        self.rect.topleft = position
+
+
+
 
 pygame.init()
 
 
 #Ouverture de la fenêtre Pygame
 
-fenetre = pygame.display.set_mode((640, 480))
+fenetre = pygame.display.set_mode((200,300))
 
 #Chargement et collage du fond
 
-fond = pygame.image.load("images.jpeg").convert()
+fond = pygame.image.load("image/images.jpeg").convert()
 
 fenetre.blit(fond, (0,0))
 
@@ -20,6 +35,13 @@ fenetre.blit(fond, (0,0))
 pygame.display.flip()
 
 continuer = 1
+
+
+
+A = Boutton('A', (100, 550))
+fond.blit(A.image, A.rect)
+
+display.flip()
 
 while continuer:
     for event in pygame.event.get():  # Attente des événements
