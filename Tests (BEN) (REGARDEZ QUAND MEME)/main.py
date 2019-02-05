082c112a10dd30e1eaff86ded73257920bd505d9
arnoultg <gregoire.arnoult@free.fr>
2019-02-05 14:54:19 +0100

    opj

--- Tests (BEN) (REGARDEZ QUAND MEME)/main.py
+++ Tests (BEN) (REGARDEZ QUAND MEME)/main.py
@@ -74,24 +74,29 @@ class plateforme(object):
 
         if rect[0] + rect[2] > self.hitbox[0] and rect[0] < self.hitbox[0] + self.hitbox[2]:
             if rect[1] < self.hitbox[3] +self.hitbox[1] and rect[1] + rect[3] > self.hitbox[1]:
-
-                if rect[0] < self.hitbox[0]:  #collision a droite
+                print("1 : ",rect[1] + rect[3])
+                print("2 : ",self.hitbox[1] + self.hitbox[3]/2)
+                print("3 : ",self.hitbox[1] -50)
+                print("\n")
+                """if rect[0] < self.hitbox[0]:  #collision a droite
                     poele.x -= 5
 
                 elif rect[0] + rect[2] > self.hitbox[0]:
-                    poele.x += 5
+                    poele.x += 5"""
 
+                if ((rect[1] + rect[3]) <= (self.hitbox[1] + self.hitbox[3]/2)) and ((rect[1] + rect[3]) >= (self.hitbox[1] -50)) :
+                    poele.y = self.hitbox[1] - 10
+                    print("yeeeeeeeeeees",self.hitbox[1])
+                    poele.isJump = False
+                    poele.jumpCount = 10
 
-                if rect[1] <= self.hitbox[1] +self.hitbox[3]:
+                """if rect[1] <= self.hitbox[1] + self.hitbox[3]//2:
                     poele.y = self.hitbox[1]+self.hitbox[3] -5
                     poele.isJump = False
-                    poele.jumpCount = 10
+                    poele.jumpCount = 10"""
 
 
-                elif rect[1] + rect[3] > self.hitbox[1]:
-                    poele.y = self.hitbox[1] + 5
-                    poele.isJump = False
-                    poele.jumpCount = 10
+
 
                 return True
         return False
