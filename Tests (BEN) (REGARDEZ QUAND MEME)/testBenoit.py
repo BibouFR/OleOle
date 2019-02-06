643c4d99bdb6d4be5b12f72a0674e139e3156ee1
perrbeno <perrier.benoit@yahoo.com>
2019-02-06 15:36:20 +0100

    a

--- Tests (BEN) (REGARDEZ QUAND MEME)/testBenoit.py
+++ Tests (BEN) (REGARDEZ QUAND MEME)/testBenoit.py
@@ -4,9 +4,9 @@ from random import *
 
 class Crepes:
     def __init__(self, difficulte):        #Crée une crêpe en fonction de la difficulté renvoie une liste d'ingrédients
-        ingredientsDisponibles = ["Oeuf","Fromage","Jambon","Cornichon", "Champignons", "Salade", "Tomate", "Nutella", "Sucre", "Miel", "Confiture", "Citron", "chantilly"]
+        ingredientsDisponibles = ["Oeuf","Fromage","Jambon","Cornichon", "Champignons", "Salade", "Tomate", "Nutella", "Sucre", "Miel", "Confiture", "Citron", "Chantilly"]
         ingredientsSales = ingredientsDisponibles[:7]
-        ingredientsSucres = ingredientsDisponibles[6:]
+        ingredientsSucres = ingredientsDisponibles[7:]
         self.ingredients = []
         self.difficulte = difficulte
         a = choice(ingredientsDisponibles)
@@ -41,22 +41,66 @@ class Crepes:
         for i in range(0,len(self.ingredients)):
             print("\t- "+self.ingredients[i])
 
-def tableau(win, hitbox):
-    pygame.draw.rect(win, (255,0,0), hitbox, 2)
+def rectangle(win, couleur, hitbox):
+    pygame.draw.rect(win, couleur, hitbox, 2)
+
+def AfficheRecette(win, x, y, crepe):
+    #COULEURS
+    blanc = (255,255,255)
+    rouge = (255,0,0)
+    vert = (0,255,0)
+    jaune = (255,215,0)
+
+    #GESTION DES DIFFICULTES
+    if crepe.difficulte==1:
+        difstr="Normal"
+        couldif = jaune
+    elif crepe.difficulte>=2:
+        difstr="Difficile"
+        couldif = rouge
+    else:
+        difstr="Facile"
+        couldif = vert
+
+    #DIMENSIONS + TEXTES
+    crect1 = (x,y,150,50)
+    crect2 = (x, y+50, 150,50*len(crepe.ingredients))
+    rectangle(win, blanc, crect1)
+    rectangle(win, blanc, crect2)
+
+    police = pygame.font.SysFont("freesans",20)
+
+    texte = police.render(difstr, True, couldif)
+    positDiff = texte.get_rect()
+    positDiff.centerx = x+crect1[2]/2
+    positDiff.centery = y+crect1[3]/2
+
+    win.blit(texte, positDiff)
+
+    nb = 25
+    for ing in crepe.ingredients:
+        texte = police.render(ing, True, blanc)
+        position = texte.get_rect()
+        position.centerx = crect2[0]+crect2[2]/2
+        position.centery = crect2[1]+nb
+        win.blit(texte, position)
+        nb += 50
+
+    pygame.display.flip()
+
 
 pygame.init()
 winwidth=500
 winheight=400
 win = pygame.display.set_mode((winwidth,winheight))
 
-testcr = Crepes(1)
-print(testcr.ingredients)
-testcr.afficherCrepe()
+testcrfacile = Crepes(0)
+testcrmoy = Crepes(1)
+testcrdif = Crepes(2)
 
-hitbox = (100,100,100,100)
-
-pygame.draw.rect(win, (255,255,255), hitbox, 2)
-pygame.display.flip()
+AfficheRecette(win, 0,0, testcrfacile)
+AfficheRecette(win, 150, 0, testcrmoy)
+AfficheRecette(win, 300, 0, testcrdif)
 
 a=True
 while a:
