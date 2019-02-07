5b4672b3fe42ed04365501cc69a3b253196c3fff
perrbeno <perrier.benoit@yahoo.com>
2019-02-07 20:24:49 +0100

    a

--- Crepe_Party/accueil.py
+++ Crepe_Party/accueil.py
@@ -48,6 +48,7 @@ def AfficheAccueil(tailleAccueil):
     xBaseBoutons = 390
     yBaseBoutons = 340
     crectBaseBoutons = (xBaseBoutons, yBaseBoutons, 250, 70)
+
     boutonJouer = pygame.draw.rect(accueil, gris, crectBaseBoutons, 1)
     taillePolice = 40
     policeNomJouer = pygame.font.SysFont("freesans", taillePolice)
@@ -59,43 +60,43 @@ def AfficheAccueil(tailleAccueil):
 
     crectBaseBoutons = incr(crectBaseBoutons)
     boutonHighscores = pygame.draw.rect(accueil, gris, crectBaseBoutons, 1)
-    taillePolice = 60
-    policeNomJeu = pygame.font.SysFont("freesans", taillePolice)
-    texteJeu = policeNomJeu.render("Crêpe Party !", True, noir)
-    positTexteJeu = texteJeu.get_rect()
-    positTexteJeu.centerx = crectNomJeu[0]+crectNomJeu[2]/2
-    positTexteJeu.centery = crectNomJeu[1]+crectNomJeu[3]/2
-    accueil.blit(texteJeu, positTexteJeu)
+    policeNomHighscores = pygame.font.SysFont("freesans", taillePolice)
+    texteHighscores = policeNomHighscores.render("Scores", True, noir)
+    positTexteHighscores = texteHighscores.get_rect()
+    positTexteHighscores.centerx = crectBaseBoutons[0]+crectBaseBoutons[2]/2
+    positTexteHighscores.centery = crectBaseBoutons[1]+crectBaseBoutons[3]/2
+    accueil.blit(texteHighscores, positTexteHighscores)
 
     crectBaseBoutons = incr(crectBaseBoutons)
     boutonRegles = pygame.draw.rect(accueil, gris, crectBaseBoutons, 1)
-    taillePolice = 60
-    policeNomJeu = pygame.font.SysFont("freesans", taillePolice)
-    texteJeu = policeNomJeu.render("Crêpe Party !", True, noir)
-    positTexteJeu = texteJeu.get_rect()
-    positTexteJeu.centerx = crectNomJeu[0]+crectNomJeu[2]/2
-    positTexteJeu.centery = crectNomJeu[1]+crectNomJeu[3]/2
-    accueil.blit(texteJeu, positTexteJeu)
+    policeNomRegles = pygame.font.SysFont("freesans", taillePolice)
+    texteRegles = policeNomRegles.render("Règles", True, noir)
+    positTexteRegles = texteRegles.get_rect()
+    positTexteRegles.centerx = crectBaseBoutons[0]+crectBaseBoutons[2]/2
+    positTexteRegles.centery = crectBaseBoutons[1]+crectBaseBoutons[3]/2
+    accueil.blit(texteRegles, positTexteRegles)
 
     crectBaseBoutons = incr(crectBaseBoutons)
     boutonCredits = pygame.draw.rect(accueil, gris, crectBaseBoutons, 1)
-    taillePolice = 60
-    policeNomJeu = pygame.font.SysFont("freesans", taillePolice)
-    texteJeu = policeNomJeu.render("Crêpe Party !", True, noir)
-    positTexteJeu = texteJeu.get_rect()
-    positTexteJeu.centerx = crectNomJeu[0]+crectNomJeu[2]/2
-    positTexteJeu.centery = crectNomJeu[1]+crectNomJeu[3]/2
-    accueil.blit(texteJeu, positTexteJeu)
+    policeNomCredits = pygame.font.SysFont("freesans", taillePolice)
+    texteCredits = policeNomCredits.render("Crédits", True, noir)
+    positTexteCredits = texteCredits.get_rect()
+    positTexteCredits.centerx = crectBaseBoutons[0]+crectBaseBoutons[2]/2
+    positTexteCredits.centery = crectBaseBoutons[1]+crectBaseBoutons[3]/2
+    accueil.blit(texteCredits, positTexteCredits)
 
     crectBaseBoutons = incr(crectBaseBoutons)
     boutonQuitter = pygame.draw.rect(accueil, gris, crectBaseBoutons, 1)
-    taillePolice = 60
     policeNomJeu = pygame.font.SysFont("freesans", taillePolice)
-    texteJeu = policeNomJeu.render("Crêpe Party !", True, noir)
+    texteJeu = policeNomJeu.render("Quitter", True, noir)
     positTexteJeu = texteJeu.get_rect()
-    positTexteJeu.centerx = crectNomJeu[0]+crectNomJeu[2]/2
-    positTexteJeu.centery = crectNomJeu[1]+crectNomJeu[3]/2
+    positTexteJeu.centerx = crectBaseBoutons[0]+crectBaseBoutons[2]/2
+    positTexteJeu.centery = crectBaseBoutons[1]+crectBaseBoutons[3]/2
     accueil.blit(texteJeu, positTexteJeu)
+
+
+
+
 
 
     pygame.display.flip()
@@ -104,10 +105,27 @@ def AfficheAccueil(tailleAccueil):
     #    LancerJeu()
     run = True
     while run:
+        mouseXY = pygame.mouse.get_pos()
+        over_Jouer = boutonJouer.collidepoint(mouseXY)
+        over_Highscores = boutonHighscores.collidepoint(mouseXY)
+        over_Regles = boutonRegles.collidepoint(mouseXY)
+        over_Credits = boutonCredits.collidepoint(mouseXY)
+        over_Quitter = boutonQuitter.collidepoint(mouseXY)
+
         for event in pygame.event.get():
-            if event.type == pygame.QUIT:
+            if event.type == pygame.QUIT or event.type == pygame.MOUSEBUTTONDOWN and event.button==1 and over_Quitter:
                 print("Merci d'avoir joué, à bientot !")
                 run = False
+            elif event.type == pygame.MOUSEBUTTONDOWN and event.button==1 and over_Jouer:
+                print("LOADING...")
+                LancerJeu()
+            elif event.type == pygame.MOUSEBUTTONDOWN and event.button==1 and over_Regles:
+                print("REGLES")
+            elif event.type == pygame.MOUSEBUTTONDOWN and event.button==1 and over_Credits:
+                import accueilOuvert.credits
+            elif event.type == pygame.MOUSEBUTTONDOWN and event.button==1 and over_Highscores:
+                import accueilOuvert.highscores
+
     pygame.display.flip()
 
 
