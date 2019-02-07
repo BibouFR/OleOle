103f0f0e6bd512e879fce97a18ced46fc7b9872f
arnoultg <gregoire.arnoult@free.fr>
2019-02-07 18:33:07 +0100

    version buger sur le monde lunaire

--- Crepe_Party/main.py
+++ Crepe_Party/main.py
@@ -241,6 +241,7 @@ def redrawGameWindow():
     global seconds
     global score
     global modelunaire
+    global timelunaire
 
     time = 180 - math.floor(seconds)
     timer = "Time : " + str(time)
@@ -267,17 +268,17 @@ def redrawGameWindow():
 
         win.blit(texttemps, [((bg.get_width()/3)), 50])
         win.blit(textscore, [((bg.get_width()/4) - 150), 50])
-        pygame.display.update()
         AfficheRecette(win,0,0,testcr, (True, testcr.ingredients[1]))
+        pygame.display.update()
     else:
-        print(modelunaire)
         win.blit(bglune, (bgXlune,0))
         win.blit(bglune, (bgX2lune,0))
         win.blit(texttemps, [((bg.get_width()/3)), 50])
         win.blit(textscore, [((bg.get_width()/4) - 150), 50])
 
-        timelunaire = 0
-        timelunaire =(math.floor(seconds)) - (math.floor(seconds)-20)
+        start_ticks=pygame.time.get_ticks()
+        debut =(pygame.time.get_ticks()-start_ticks)/1000
+        timelunaire = 20 - math.floor(debut)
         timerlunaire = "lunaire : " + str(timelunaire)
         font3 = pygame.font.Font(None, 50)
         texttimer = font3.render(timerlunaire, True,(255,255,255))
@@ -289,7 +290,10 @@ def redrawGameWindow():
             x.draw(win)
         if (timelunaire <= 0):
             modelunaire = False
+            debut = 0
+            timelunaire = 0
 
+        AfficheRecette(win,0,0,testcr, (True, testcr.ingredients[1]))
         pygame.display.update()
         #poele.draw(win)
         #sol.draw(win)
@@ -303,7 +307,6 @@ ingredientsDisponibles = ["Oeuf","Fromage","Jambon","Cornichon", "Champignons", 
 
 score = 0
 #nbIngredients = 0
-
 plateformeSpeed = 4.4   #14.4 pour du rapide
 plateformeSpawn = 2750  #1000 pour du rapide
 
